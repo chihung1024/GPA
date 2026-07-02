f2bd8961b691f9d76f29e46ec7e4f7c11d9df75f
chihung1024 <104315542+chihung1024@users.noreply.github.com>
2026-07-02 11:17:50 +0800

    Delete functions directory

--- functions/quiz-content.js
+++
@@ -1,6 +0,0 @@
-export function onRequestGet(context) {
-  const target = new URL("/", context.request.url);
-  target.search = new URL(context.request.url).search;
-  target.hash = new URL(context.request.url).hash;
-  return Response.redirect(target.toString(), 302);
-}
